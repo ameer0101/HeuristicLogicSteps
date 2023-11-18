ea84f11e75f149d2a9115829ee69d414a5e66853
Ameer <ameermatr@yahoo.com>
2023-11-18 13:56:11 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,91 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import Model.Rules;
+import Model.State;
+
 public class Main {
+    // Hey
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        List<State> gameStates = new ArrayList<>();
+        List<State> goalState = new ArrayList<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Board2.txt"));
+            int row = Integer.parseInt(reader.readLine());
+            int col = Integer.parseInt(reader.readLine());
+
+            String line;
+            int rowIndex = 0;
+            int[][] board = new int[row][col];
+
+            int numStartPoints = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < numStartPoints; i++) {
+                String[] startPoint = reader.readLine().split(" ");
+                int startRow = Integer.parseInt(startPoint[0]);
+                int startCol = Integer.parseInt(startPoint[1]);
+                int cost = Integer.parseInt(startPoint[2]);
+                State state = new State(row, col, startRow, startCol, board,cost);
+                gameStates.add(state);
+            }
+
+            while((line = reader.readLine()) != null) {
+                String[] values = line.split(" ");
+                if (values.length != col) {
+                    throw new IllegalArgumentException("Invalid number of values on line: " + line);
+                }
+                for (int i = 0; i < values.length; i++) {
+                    board[rowIndex][i] = Integer.parseInt(values[i]);
+                }
+                rowIndex++;
+            }
+
+
+
+            while (Rules.isfinal(board,row,col)) {
+
+                for (State currentState : gameStates) {
+                    System.out.println("Player location: (" + currentState.startRow + ", " + currentState.startCol + ")");
+                    if (Rules.isDead(currentState)) {
+
+                        System.out.printf("You Lose !");
+                        System.exit(0);
+                    }
+                }
+
+
+                System.out.println("Enter the direction to move :");
+                String direction = scanner.next();
+
+                switch (direction.toLowerCase()) {
+                    case "n":
+                        System.out.println("Number of states in the queue: " + gameStates.size());
+                        break;
+
+//                    case "ucs":
+//                        UCS ucs = new UCS(gameStates.get(0),new ArrayList<>(gameStates), new ArrayList<>());
+//                        List<State> ucsStates = ucs.search();
+//                        gameStates.addAll(ucsStates);
+//
+//                        break;
+
+
+
+
+                    default:
+                        System.out.println("Invalid direction. Try again.");
+                }
+
+            }
+            System.out.println("You Win !");
+        }
+
+        catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
